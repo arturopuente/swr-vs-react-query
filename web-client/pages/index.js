31af454df10a0d076787a48a68413f889cc08210
Arturo Puente <arturopuentevc@gmail.com>
2020-08-17 11:20:57 -0500

    Render virtual lists for both sections

--- web-client/pages/index.js
+++ web-client/pages/index.js
@@ -1,65 +1,70 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React from "react";
+import { useVirtual } from "react-virtual";
+import styles from "../styles/Home.module.css";
+
+function List(items = []) {
+  const parentRef = React.useRef();
+
+  const rowVirtualizer = useVirtual({
+    size: 1000,
+    parentRef,
+    estimateSize: React.useCallback(() => 35, [])
+  });
+
+  return (
+    <div ref={parentRef} className={styles.list}>
+      <div
+        className="ListInner"
+        style={{
+          height: `${rowVirtualizer.totalSize}px`,
+          width: "100%",
+          position: "relative"
+        }}
+      >
+        {rowVirtualizer.virtualItems.map(virtualRow => (
+          <div
+            key={virtualRow.index}
+            className={virtualRow.index % 2 ? "ListItemOdd" : "ListItemEven"}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: `${virtualRow.size}px`,
+              transform: `translateY(${virtualRow.start}px)`
+            }}
+          >
+            Row {virtualRow.index}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>SWR vs React-Query</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section>
+          <h1 className={styles.title}>SWR</h1>
+          <p className={styles.description}>
+            <List />
+          </p>
+        </section>
+        <section>
+          <h1 className={styles.title}>React-Query</h1>
+          <p className={styles.description}>
+            <List />
+          </p>
+        </section>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
